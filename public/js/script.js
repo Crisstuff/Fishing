7712dcb8403597bc43a29d1bcf397ac8964cab1a
Aksel <aknea001@osloskolen.no>
2024-11-27 13:04:01 +0100

    fixing small things w the website

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,3 @@
-const mysql = require("mysql")
-
-function testMysql() {
-    const db = mysql.createConnection({
-        host: "192.168.1.46",
-        user: "scammer",
-        password: process.env.mysqlPass
-    })
-}
-
 function handleFormSubmit(event) {
     event.preventDefault(); // Prevents the default form submission behavior (page refresh)
     
@@ -101,6 +91,40 @@ function checkValue() {
     }
 }
 
-function redirect(){
-    window.location = "https://finn.no"
+function passVis() {
+    const inputPass = document.getElementById("password")
+    const eye = document.getElementById("passEye")
+    console.log(eye)
+
+    if (inputPass.type == "password") {
+        inputPass.type = "text"
+    } else {
+        inputPass.type = "password"
+    }
+
+    if (eye.getAttribute("d") == "M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z") {
+        eye.setAttribute("d", "M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z")
+    } else {
+        eye.setAttribute("d", "M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z")
+    }
 }
+
+function rememberCheck() {
+    const checkbox = document.getElementById("rememberCheck")
+    const ripple = document.getElementById("MuiTouchRipple-root")
+    const span1 = document.createElement("span")
+    const span2 = document.createElement("span")
+
+    span1.classList.add("MuiTouchRipple-ripple")
+
+    if (checkbox.getAttribute("d") == "M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z") {
+        checkbox.setAttribute("d", "M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z")
+    } else {
+        checkbox.setAttribute("d", "M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z")
+    }
+}
+
+<span class="MuiTouchRipple-root">
+    <span class="MuiTouchRipple-ripple MuiTouchRipple-rippleVisible" style="width: 50.0469px; height: 50.0469px; top: -0.0234375px; left: -0.0234375px;"></span>
+    <span class="MuiTouchRipple-child MuiTouchRipple-childLeaving"></span>
+</span>
